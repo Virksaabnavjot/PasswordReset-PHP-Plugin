208b7aa85ed6e1fe156731a9ef0c0fa1de04451c
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-07-03 22:58:10 +0100

    Creating the form

--- features/forgot_password.php
+++ features/forgot_password.php
@@ -1,3 +1,13 @@
 <?php
 echo "Test Hello, forgot Password "
+//when the submit button is pressed this code will execute
+if(isset($_POST["forgotPassword"])){
+
+}
  ?>
+
+ <form action="index.php" method="POST">
+ To reset your password, please enter your email in the text box below:
+   <input type="text" name="email" size="200" placeholder="Email"/>
+   <input type="submit" name="forgotPassword" value=" Submit " />
+ </form>
