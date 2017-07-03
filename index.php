9495bb22c88ff3957c704fa0c43c15cbe66f34ec
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-07-03 22:53:33 +0100

    Adding the forgot password file
    And testing if its working then continue

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include("includes/header.php");
 setPageTitleAndDescription("Forgot Password", "This is a basic Password reset feature");
+include("features/forgot_password.php");
 include("includes/footer.php");
 ?>
