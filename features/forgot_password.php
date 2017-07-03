f28acd71152f8dc076dbd756a88a153a2070903a
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-07-03 23:24:40 +0100

    check if user input is correct
    Next: If the user entered a correct email check if exist in our
    database if yes send a reset link else notify user the email does’t
    exist, consider signup

--- features/forgot_password.php
+++ features/forgot_password.php
@@ -2,12 +2,19 @@
 echo "Test Hello, forgot Password "
 //when the submit button is pressed this code will execute
 if(isset($_POST["forgotPassword"])){
-
+  //make connection to the database
+  include("db_connection.php");
+  //validate the user input and check if its an actual email
+  if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    echo "The Email address is not valid, please check again.";
+  }else{
+    //logic here
+  }
 }
- ?>
+?>
 
- <form action="index.php" method="POST">
- To reset your password, please enter your email in the text box below:
-   <input type="text" name="email" size="200" placeholder="Email"/>
-   <input type="submit" name="forgotPassword" value=" Submit " />
- </form>
+<form action="index.php" method="POST">
+  To reset your password, please enter your email in the text box below:
+  <input type="text" name="email" size="200" placeholder="Email"/>
+  <input type="submit" name="forgotPassword" value=" Submit " />
+</form>
